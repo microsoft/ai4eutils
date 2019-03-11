d4d1a599e4a1bc03c16d961f8c7b459119882d61
REDMOND\dan <dan@microsoft.com>
2019-03-11 16:31:40 -0700

    Added Azure storage calculator script (using Azure Metrics)

---
+++ azure-sdk-calc-storage-size/README.md
@@ -0,0 +1,61 @@
+# Overview
+This tool will return the total blob and file sizes for all storage accounts in an Azure subscription.
+
+# Installing dependencies
+
+```
+pip install azure==4.0.0 
+pip install pandas==0.24.1
+pip install --upgrade pip enum34
+pip install humanfriendly
+```
+
+# Running the application
+
+Run the following command:
+
+```
+python metrics-data.py
+```
+
+The application will ask you to log in; the following page will open in a browser:
+
+
+![login](docs/device-login1.jpg)
+
+
+Copy the <b>input code</b> from the browser URL:
+
+
+![login](docs/device-login2.jpg)
+
+
+Paste the <b>input code</b> in theto code field:
+
+
+![login](docs/device-login3.jpg)
+
+
+Click "continue", after which you might be asked to log in again.  Once you see the following message in the browser, return to the command prompt:
+
+
+![login](docs/device-login4.jpg)
+
+
+Next you will be prompted to enter the name of the subscription that you would like to examine.  If the application was unable to find a subscription with the subscription name entered, you will be prompted to re-enter the subscription name. If the application was able to find a matching subscription it will continue with execution.
+
+
+In your command prompt you should see the script enumerating storage accounts.
+
+
+# Review results
+
+Once the code has run successfully you should see a message like &ldquo;Metrics saved to file: metrics_03-06-19-125503.csv&rdquo;.
+
+The metrics file will be saved in the same directory as the code.  Columns will be:
+
+`Storage account, Resource group, Blob capacity, File capacity, Total capacity`
+
+...with capacity values specified in bytes.  These represent the average storage consumed over the last 24 hours by each storage account, as recorded by Azure Metrics.
+
+
---
+++ azure-sdk-calc-storage-size/metrics-data.py
@@ -0,0 +1,147 @@
+#%% Constants and imports
+
+import utils
+import datetime
+import pandas as pd
+import humanfriendly
+import math
+import os
+
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.monitor import MonitorManagementClient
+from azure.mgmt.storage import StorageManagementClient
+
+from enum import Enum
+
+class Authentication:
+
+    # This refers to the on.microsoft.com directory, change this to enumerate alternative
+    # directories.
+    tenant = '72f988bf-86f1-41af-91ab-2d7cd011db47'
+    
+    # This is the client ID for the Azure CLI
+    client_id = '04b07795-8ddb-461a-bbee-02f9e1bf7b46'
+
+METRICS_NOT_AVAILABLE = float('nan')
+
+class Metric_type(Enum): 
+
+    blob_capacity = 1
+    fileshare_capacity = 2
+
+
+#%% Classes and functions
+
+def get_used_avg_blob_capacity(credentials,subscription_id):
+    
+    resource_client = ResourceManagementClient(credentials, subscription_id)
+    storage_client = StorageManagementClient(credentials, subscription_id)
+    
+    lst = []
+    count = 0
+    resource_groups = resource_client.resource_groups.list()
+
+    for group in resource_groups:        
+        
+        storage_accounts = storage_client.storage_accounts.list_by_resource_group(group.name)
+        
+        for storage_account in storage_accounts:
+
+            print("Reading metric data from storage account: " + storage_account.name)
+            count += 1
+            
+            blob_size = get_metric_data_capacity(group.name, storage_account.name, 
+                                        subscription_id, Metric_type.blob_capacity)
+            file_size = get_metric_data_capacity(group.name, storage_account.name, 
+                                        subscription_id, Metric_type.fileshare_capacity)
+            
+            total_size = blob_size + file_size
+            
+            if math.isnan(total_size):
+                total_size_friendly = ''
+            else:
+                total_size_friendly = humanfriendly.format_size(total_size)
+            
+            lst.append([storage_account.name, group.name, blob_size, file_size, total_size, total_size_friendly])
+            
+    print("Total number of storage accounts: "+ str(count))
+    cols = ['Storage account', 'Resource group', 'Blob capacity', 'File capacity', 'Total capacity', 'Total capacity (friendly)']
+    df = pd.DataFrame(lst, columns=cols)
+    
+    file_name = 'metrics_' + datetime.datetime.now().strftime('%m-%d-%y-%H%M%S') + '.csv'
+    df.to_csv(file_name, header=cols, index=False)
+    print("\n")
+    print("Metrics saved to file: " + file_name)
+    return file_name
+        
+
+def get_metric_data_capacity(resource_group_name, storage_account_name, subscription_id, type):
+    
+    client = MonitorManagementClient(credentials, subscription_id)
+
+    today = datetime.datetime.utcnow().date()
+    yesterday = today - datetime.timedelta(days=1)
+
+    resource_id = (
+            "subscriptions/{}/"
+            "resourceGroups/{}/"
+            "providers/Microsoft.Storage/storageAccounts/{}/{}")
+    
+    metrics_data = None
+        
+    if (type == Metric_type.blob_capacity):
+        
+        resource_id = resource_id.format(subscription_id, 
+            resource_group_name, storage_account_name, 'blobServices/default')
+        
+        metrics_data = client.metrics.list(
+            resource_id,
+            timespan="{}/{}".format(yesterday, today),
+            interval='PT1H',
+            metric='Blob capacity',
+            aggregation='Average')
+
+    if (type == Metric_type.fileshare_capacity):    
+            
+        resource_id = resource_id.format(subscription_id, 
+        resource_group_name, storage_account_name, 'fileServices/default')
+        
+        metrics_data = client.metrics.list(
+            resource_id,
+            timespan="{}/{}".format(yesterday, today),
+            interval='PT1H',
+            metric='File capacity',
+            aggregation='Average')
+    
+    if(metrics_data.value is None):
+        
+        return METRICS_NOT_AVAILABLE
+    
+    for item in metrics_data.value:
+        
+        for item in item.timeseries:
+            if(len(item.data) > 0):
+                data = item.data[-1]
+                if(data.average is not None):
+                    return data.average
+                else:
+                    return METRICS_NOT_AVAILABLE
+
+    return METRICS_NOT_AVAILABLE     
+
+# ... def get_metric_data_capacity
+    
+
+#%% Command-line driver
+    
+if __name__ == '__main__':
+
+     #%%
+     
+     auth = Authentication()
+     credentials = utils.authenticate_device_code(auth)
+     
+     subscription_id =  utils.get_subscription_id(credentials)
+    
+     file_name = get_used_avg_blob_capacity(credentials,subscription_id)
+     os.startfile(file_name)
