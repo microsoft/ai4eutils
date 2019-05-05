ae797e5014e1bb817dc7088bc4601366bf912e54
Dan Morris <dmorris@cs.stanford.edu>
2019-05-05 14:58:43 -0700

    Added image-related tools to path_utils

--- path_utils.py
+++ path_utils.py
@@ -4,10 +4,15 @@
 # Miscellaneous useful utils for path manipulation, things that could *almost*
 # be in os.path, but aren't.
 #
-# Owner: Dan Morris (dan@microsoft.com)
 #
 
+#%% Constants and imports
+
 import os
+import glob
+
+
+#%% General path functions
 
 def recursive_file_list(baseDir, bConvertSlashes=True):
     """
@@ -40,3 +45,44 @@ def split_path(path, maxdepth=100):
     return split_path(head, maxdepth - 1) + [ tail ] \
         if maxdepth and head and head != path \
         else [ head or tail ]
+        
+        
+#%% Image-related path functions
+        
+imageExtensions = ['.jpg','.jpeg','.gif','.png']
+    
+def is_image_file(s):
+    '''
+    Check a file's extension against a hard-coded set of image file extensions    '
+    '''
+    ext = os.path.splitext(s)[1]
+    return ext.lower() in imageExtensions
+    
+    
+def find_image_strings(strings):
+    '''
+    Given a list of strings that are potentially image file names, look for strings
+    that actually look like image file names (based on extension).
+    '''
+    imageStrings = []
+    bIsImage = [False] * len(strings)
+    for iString,f in enumerate(strings):
+        bIsImage[iString] = is_image_file(f) 
+        if bIsImage[iString]:
+            imageStrings.append(f)
+        
+    return imageStrings
+
+    
+def find_images(dirName,bRecursive=False):
+    '''
+    Find all files in a directory that look like image file names
+    '''
+    if bRecursive:
+        strings = glob.glob(os.path.join(dirName,'**','*.*'), recursive=True)
+    else:
+        strings = glob.glob(os.path.join(dirName,'*.*'))
+        
+    imageStrings = find_image_strings(strings)
+    
+    return imageStrings
