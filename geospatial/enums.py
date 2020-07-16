b4bb8ca97c158ace313df7b400e9f315961659af
Siyu Yang <yasiyu@microsoft.com>
2020-07-15 19:36:23 -0700

    Raster label visualizer tested. Imagery visualizer drafted. Recipes moved to a separate dir.

---
+++ geospatial/enums.py
@@ -0,0 +1,13 @@
+"""
+Collection of Enum classes used in /geospatial.
+"""
+
+from enum import Enum
+
+
+class ExperimentConfigMode(Enum):
+    """Which mode is the current experiment config file in, e.g. training, scoring, etc mode"""
+    PREPROCESSING = 1
+    TRAINING = 2
+    EVALUATION = 3  # different from SCORING as it usually only involves labeled tiles in the validation set
+    SCORING = 4
